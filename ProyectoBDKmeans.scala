195cbbdd838cbf5e9a87823127132fa3ac6e6480
EdgarCastillo <edgar.castillo@tectijuana.edu.mx>
2018-11-30 05:41:06 -0800

    subida de codigo

---
+++ ProyectoBDKmeans.scala
@@ -0,0 +1,38 @@
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+import org.apache.spark.ml.clustering.BisectingKMeans
+import org.apache.spark.sql.SparkSession
+import spark.implicits._
+import org.apache.spark.sql.Column
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.feature.IndexToString
+import org.apache.spark.ml.Pipeline
+
+val df = spark.read.option("inferSchema","true").csv("IRIS.csv").toDF("SepalLength", "SepalWidth", "PetalLength", "PetalWidth","class")
+val newcol = when($"class".contains("Iris-setosa"), 1.0).otherwise(when($"class".contains("Iris-virginica"), 3.0).otherwise(2.0))
+val newdf = df.withColumn("etiqueta", newcol)
+
+val assembler = new VectorAssembler().setInputCols(Array("SepalLength", "SepalWidth", "PetalLength", "PetalWidth","etiqueta")).setOutputCol("features")
+val features = assembler.transform(newdf)
+
+
+// Trains a k-means model.
+val kmeans = new KMeans().setK(3).setSeed(1)
+val model = kmeans.fit(features)
+
+// Make predictions
+val predictions = model.transform(features)
+
+// Evaluate clustering by computing Silhouette score
+val evaluator = new ClusteringEvaluator()
+
+val silhouette = evaluator.evaluate(predictions)
+println(s"Silhouette with squared euclidean distance = $silhouette")
+
+val WSSE = model.computeCost(features)
+println(s"Within set sum of Squared Errors = $WSSE")
+// Shows the result.
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
